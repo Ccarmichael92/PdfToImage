c8f56748b4ee98df42f739a9012c2f9b1d12a726
Christopher Carmichael <ccarmichael92@gmail.com>
2023-10-05 20:46:09 -0700

    Remove unused methods, clean up

--- PdfSplit/PdfSplit.cs
+++ PdfSplit/PdfSplit.cs
@@ -1,39 +1,27 @@
 ﻿using PdfiumViewer;
-using PdfSharp.Pdf;
 using PdfSharp.Pdf.IO;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using PdfDocument = PdfSharp.Pdf.PdfDocument;
 
 namespace DmsApp.Helper
 {
     public static class PdfSplit
     {
-
-        private static List<PdfDocument> SplitPdf(PdfDocument document)
+        public static List<Bitmap> Split(string document, ImageFormat format)
         {
-            List<PdfDocument> returnList = new List<PdfDocument>();
-
-            var pageCount = document.PageCount;
-
-            var index = 0;
-            while (index < pageCount)
+            PdfDocument doc = PdfReader.Open(document, PdfDocumentOpenMode.Import);
+            if(doc != null)
             {
-                PdfDocument temp = new PdfDocument();
-
-                temp.AddPage(document.Pages[index]);
-
-                returnList.Add(temp);
-
-                index++;
+                var result = Pdf2ImageList(doc);
+                return result;
             }
+            
 
-            return returnList;
+            return null;
         }
 
-        private static List<Bitmap> ImageSplitPdf(PdfDocument document)
+        private static List<Bitmap> Pdf2ImageList(PdfDocument document)
         {
             List<Bitmap> returnList = new List<Bitmap>();
 
@@ -58,59 +46,10 @@ namespace DmsApp.Helper
                     returnList.Add(bitmapImage);
                 }
 
-                //var image = RenderPage(temp, 1, size);
-
                 index++;
             }
 
             return returnList;
-        }
-
-        private static List<string> SplitString(PdfDocument document)
-        {
-            List<string> returnList = new List<string>();
-
-            var pageCount = document.PageCount;
-
-            var index = 0;
-            while (index < pageCount)
-            {
-                PdfDocument temp = new PdfDocument();
-
-                temp.AddPage(document.Pages[index]);
-
-                var newPath = Path.GetTempFileName();
-                newPath = Path.ChangeExtension(newPath, "pdf");
-
-                temp.Save(newPath);
-
-                returnList.Add(newPath);
-
-                index++;
-            }
-
-            return returnList;
-
-        }
-
-        public static List<Bitmap> Split(string document, SplitOption option)
-        {
-            PdfDocument doc = PdfReader.Open(document, PdfDocumentOpenMode.Import);
-
-            if (option == SplitOption.PdfList)
-            {
-                //return SplitPdf(doc);
-            }
-            else if (option == SplitOption.PdfStringList)
-            {
-                //return SplitString(doc);
-            }
-            else if (option == SplitOption.ImageList)
-            {
-                return ImageSplitPdf(doc);
-            }
-
-            return null;
         }
 
         private static Image GetPageImage(int pageNumber, Size size, PdfiumViewer.PdfDocument document, int dpi)
@@ -122,8 +61,6 @@ namespace DmsApp.Helper
         {
             using (var document = PdfiumViewer.PdfDocument.Load(pdfPath))
             {
-
-
                 var image = GetPageImage(pageNumber, size, document, 150);
                 {
                     return image;
@@ -135,13 +72,5 @@ namespace DmsApp.Helper
 
 
 
-    }
-
-    public enum SplitOption
-    {
-        PdfList,
-        PdfStringList,
-        ImageList,
-        ImageStringList
     }
 }
